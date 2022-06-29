c2bb607d1848d24b01ac030f08ea9c95cfe4f28f
Antoine Chou <Antoine.Chou@harmonicinc.com>
2022-06-29 17:33:34 +0200

    Add directive + tests

--- biblioWeb/src/main/webapp/js/common.js
+++ biblioWeb/src/main/webapp/js/common.js
@@ -2,35 +2,66 @@
  * 
  */
 
-
-function ajouterLivre() {
-
-}
-
-
 function controleurPrincipal($scope, $rootScope) {
     $scope.titreSite = "biblio Web\n";
+
+    $scope.$on("utilisateurInscritEvent", function (evt, utilisateur) {
+        $scope.$broadcast("majRecapEvent", utilisateur);
+    });
 }
 
-function controleurInscription($scope) {
+function controleurHeader($scope) {
+    $scope.utilisateur = "";
+    $scope.$on("utilisateurInscritEvent", function (evt, utilisateur) {
+        $scope.utilisateur = utilisateur;
+    });
+}
+/**
+Controleur de l inscription
+ */
+function controleurInscription($scope, $location) {
     $scope.titrePage = "Inscription";
+    $scope.utilisateur = { nom: "coucou", prenom: "", mdp: "", nbrEnfants: [] };
+    $scope.nbrs = [];
+    $scope.nbrEnfants = "";
+    for (var i = 0; i < 5; i++) {
+        $scope.nbrs.push(i);
+    }
+
+    $scope.majNbrEnfants = function () {
+        $scope.utilisateur.enfants = [];
+        for (var j = 1; j <= $scope.nbrEnfants; j++) {
+            $scope.utilisateur.enfants.push({
+                nomEnfant: "",
+                prenomEnfant: ""
+            });
+        }
+    }
+    $scope.validerInscription = function () {
+        //on avertit le contrileur principal
+        $scope.$emit("utilisateurInscritEvent", $scope.utilisateur);
+
+        //on refdirige vers le bon onglet
+        $location.url("/consultation");
+        //alert($scope.inscriptionForm.utilisateur.nom.$viewValue);
+    }
 }
 
 function controleurConsultation($scope, livreService) {
 
-    livreService.getLivres().success(function(livre) {
+    livreService.getLivres().success(function (livre) {
         $scope.livres = livre;
     });
 
     $scope.titrePage = "Consultation";
 
     $scope.idSort = 'id';
-    $scope.togglerSort = function() {
+    $scope.togglerSort = function () {
         $scope.idSort = ($scope.idSort = 'id') ? '-id' : 'id';
     }
-    $scope.newLivre = {"id": '', "isbn" : "", "titre": ""};
-    $scope.ajouterLivre=function() {
-        livreService.saveLivre($scope.newLivre).success(function(livreWS) {
+    $scope.newLivre = { "id": '', "isbn": "", "titre": "" };
+    $scope.ajouterLivre = function () {
+        livreService.saveLivre($scope.newLivre).success(function (livreWS) {
             $scope.livres[$scope.livres.length] = livreWS;
         });
     }
@@ -65,15 +96,41 @@ var monAppli = angular.module("monAppli", ['ngRoute']);
 //DECLARATION DE SERVICE
 /*****************************************/
 //declaration des services (en singleton)
-monAppli.factory("livreService", function($http) {
-	return {
-		getLivres: function() {
-			return $http.get("/biblioRS/serviceRS/livre");
-		},
-        saveLivre: function(livre) {
+monAppli.factory("livreService", function ($http) {
+    return {
+        getLivres: function () {
+            return $http.get("/biblioRS/serviceRS/livre");
+        },
+        saveLivre: function (livre) {
             return $http.post("/biblioRS/serviceRS/livre", livre);
         }
-	};
+    };
+});
+
+/*****************************************/
+//DECLARATION DES DIRECTIVES
+/*****************************************/
+monAppli.directive("maDirective", function () {
+    return {
+        restrict: "EA",
+        template: "<p>Bonjour !</p>"
+    };
+});
+
+monAppli.directive("menuPrincipal", function () {
+    return {
+        restrict: "EA",
+        templateUrl: "template/menu.html",
+        replace: true
+    };
+});
+
+monAppli.directive("livres", function () {
+    return {
+        restrict: "EA",
+        template: "template/livres.html",
+        replace: true
+    };
 });
 
 
@@ -114,4 +171,5 @@ monAppli.controller("controleurContact", controleurContact);
 monAppli.controller("controleurInscription", controleurInscription);
 monAppli.controller("controleurMenu", controleurMenu);
 monAppli.controller("controleurAccueil", controleurAccueil);
+monAppli.controller("controleurHeader", controleurHeader);
 
